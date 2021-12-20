453eba3190a9920cda5fe05404fa500d93375a7c
joebywan <joseph.d.howe@gmail.com>
2021-12-20 13:33:29 +1000

    jk

--- day1/bandnamegenerator.py
+++ day1/bandnamegenerator.py
@@ -1,4 +1,6 @@
 print("Welcome to the Band Name Generator")
-city = input("What's city did you grow up in?\n")
-pet = input("What's the name of your pet?\n")
-print("Your band's name could be: " + city + " " + pet)
+# city = input("What's city did you grow up in?\n")
+# pet = input("What's the name of your pet?\n")
+# print("Your band's name could be: " + city + " " + pet)
+
+print("Your band's name could be: " + input("What's city did you grow up in?\n") + " " + input("What's the name of your pet?\n"))
